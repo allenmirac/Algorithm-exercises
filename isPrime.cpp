03e5f095b3a4a8208cd6ac3a0fd6784c37d4cc47
Allen Mirac <2049829115@qq.com>
2025-02-21 15:06:35 +0800

    :fire:isPrime埃氏筛方法

--- isPrime.cpp
+++ isPrime.cpp
@@ -10,3 +10,25 @@ bool isPrime(int n)
     }
     return true;
 }
+// 埃氏筛
+int countPrime(int n) {
+    int ans = 0;
+    vector<int> isPrime(n, 1);
+    // for(int i=2; i<n; i++) {
+    //     if(isPrime[i]) ans++;
+    //     for(long long j=i*i; j<n; j+=i) { // 这里i*i的计算还是int，计算完i*i之后才会转化为long long
+    //         isPrime[i]=0;
+    //     }
+    // }
+    for(int i = 2; i < n; i++) {
+        if(isPrime[i]) ans++;
+        if((long long)i * i >= n) continue;
+        for(long long j = (long long)i * i; j < n; j += i) {
+            isPrime[j] = 0;
+        }
+    }
+    return ans;
+}
+int main(){
+    return 0;
+}
