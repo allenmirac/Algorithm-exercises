a43ca70ebf7fc25fb13e2479509c6354a8003298
Allen Mirac <2049829115@qq.com>
2025-03-03 14:55:33 +0800

    :fire:qpow2

--- qpow.cpp
+++ qpow.cpp
@@ -2,18 +2,28 @@
 using namespace std;
 #define MOD 1000000007;
 typedef long long ll;
-ll qpow(ll a, ll n) {
+ll qpow1(ll a, ll n) {
     if(n==0) {
         return 1;
     } else if(n%2==1) {
-        return qpow(a, n-1) * a;
+        return qpow1(a, n-1) * a;
     } else {
-        ll temp = qpow(a, n/2);
+        ll temp = qpow1(a, n/2);
         return temp * temp;
     }
 }
 
+ll qpow2(int n, int i) {
+    if(i==0) return 1;
+    if(i==1) return n;
+    if(i%2==1) {
+        return n*qpow2(n*n, i/2)%MOD;
+    } else {
+        return qpow2(n*n, i/2)%MOD;
+    }
+}
+
 int main() {
-    cout << qpow(2, 20);
+    cout << qpow2(2, 20);
     return 0;
 }
